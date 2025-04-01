97a533b9a075cea23f270d3581e27b56f586b662
Jack Howard <66543516+jackkatahdin@users.noreply.github.com>
2025-04-01 10:39:51 -0400

    Update even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -5,19 +5,19 @@ import java.io.*;
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
-        file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
-        file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
-        for (int i = 1; i <= 50; i++) {
+        file(write)("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
+        file.write("<table>\ntr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
+        for (int i = 1; i <= 50; i+=) {
             if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
             }
-            else {
-                file.write("<tr><td></td><td>" + i + "</td></tr>\n");
+            else if {
+                file.write("<td><tr></td><td>" + i + "</td></tr>\n");
             }
         }
         file.write("</table>\n</body>\n</html>");
-        file.close();
-        BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
+        file.close()
+        BufferedReader reader = new BufferedReader(new FileReader("../numbers.html"));
         String line;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
